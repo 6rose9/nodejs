19f81771d49a7cc99fb8f5e7f6463058105e6f7c
Crystal <zinzinthinzaw8@gmail.com>
2025-07-14 10:30:53 +0630

    d1 : intro, global variable, status code

---
+++ l2globalvariables.js
@@ -0,0 +1,14 @@
+// Global Variable 
+
+console.log("Dir Name = " , __dirname);
+
+console.log("File Name =", __filename);
+
+const fullname = "Aung Ko Ko";
+console.log(fullname);
+
+globalThis.nickname = "Aung Aung";
+console.log(nickname);
+
+//=> process
+console.log("Node Version : ", process.version)
