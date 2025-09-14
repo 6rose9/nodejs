02d00487fcfed30c04bd5b66c4ed9345d5632662
Crystal <zinzinthinzaw8@gmail.com>
2025-09-14 10:02:41 +0630

    d6: process

--- l8process/index.js
+++ l8process/index.js
@@ -1,13 +1,39 @@
 // => process
-console.log("OS Platform : ", process.platform);
-console.log("Node Version : ", process.version);
-console.log("Version : ", process.versions);
-console.log("Architecture : ", process.arch);
-console.log("Current working directory : " , process.cwd());
-console.log("Command Line arguments : ", process.argv); // array
-console.log(`Process has been running for ${process.uptime()} seconds`);
+// console.log("OS Platform : ", process.platform);
+// console.log("Node Version : ", process.version);
+// console.log("Version : ", process.versions);
+// console.log("Architecture : ", process.arch);
+// console.log("Current working directory : " , process.cwd());
+// console.log("Command Line arguments : ", process.argv); // array
+// console.log(`Process has been running for ${process.uptime()} seconds`);
 
-console.log(process.argv); // node index.js susu 23
-const argname = process.argv[2];
-const argage = process.argv[3];
-console.log(argname,argage);
+// console.log(process.argv); // node index.js susu 23
+// const argname = process.argv[2];
+// const argage = process.argv[3];
+// console.log(argname,argage);
+
+// => Change Wroking Directory
+// console.log("Before ", process.cwd());
+// process.chdir("..");
+
+// console.log("After ", process.cwd());
+
+// => Exit the Process
+// console.log("Before Exit");
+// process.exit(0); // 0 = success, 1 = error
+// console.log("After Exit");
+
+// exe 1
+
+function todotask(){
+    if(Math.random() > 0.5){
+        console.log("Task Completed");
+        process.exit(0);
+    }else{
+        console.error("Task Failed");
+        process.exit(1);
+    }
+}
+
+todotask();
+console.log("Hello Program");
