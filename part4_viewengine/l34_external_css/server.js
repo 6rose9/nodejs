d306d2ad95cceb02c32f6ba6842ca51dfc740640
Crystal <zinzinthinzaw8@gmail.com>
2026-03-08 14:43:45 +0630

    view engine: ejs

---
+++ part4_viewengine/l34_external_css/server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+// Register view engin, Set EJS as view engine
+app.set("view engine", "ejs");
+
+// Set views folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Use Morgan for logging
+app.use(morgan('combined'));
+
+// Middleware to serve files from the public folder as the website root.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// GET route
+app.get('/', (req, res) => {
+    const posts = [
+        { id: 1, title: "Post 1", subtitle: "This is post 1", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate." },
+        { id: 2, title: "Post 2", subtitle: "This is post 2", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate." },
+        { id: 3, title: "Post 3", subtitle: "This is post 3", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate." },
+    ]
+
+    res.render('index', {
+        title: 'Home Page',
+        posts
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us'
+    });
+});
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+app.get('/posts/create', (req, res) => {
+    res.render('create', {
+        title: 'Create Post'
+    });
+});
+
+// 404 (note : This should be the last route)
+app.use((req, res) => {
+    res.status(404).render('404', {
+        title: 'Page Not Found'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+});
